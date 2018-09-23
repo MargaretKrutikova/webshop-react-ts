ac19598b425b1afa65d3828e8f8fd485d3c96a12
Margarita Krutikova <margarita.krutikova@starrepublic.com>
2018-09-23 22:05:57 +0200

    Add mock api with fake products and paging

---
+++ src/api/index.ts
@@ -0,0 +1,42 @@
+import axios from "axios"
+import { Product } from "store/products"
+
+const API_ROOT = "https://reqres.in/api"
+
+const request = axios.create({
+  baseURL: API_ROOT
+})
+
+export type ProductsApiResponse = {
+  page: number
+  per_page: number
+  total: number
+  total_pages: number
+  data: Product[]
+}
+
+const Products = {
+  getProducts: () => request.get<ProductsApiResponse>(`/products`).then(resp => resp.data.data),
+  getProductsPaginated: (page: number, itemsPerPage: number): Promise<ProductsApiResponse> =>
+    request
+      .get<ProductsApiResponse>(`/products?page=${page}&per_page=${itemsPerPage}`)
+      .then(resp => resp.data),
+  searchProducts: (
+    searchTerm: string,
+    page: number,
+    itemsPerPage: number
+  ): Promise<ProductsApiResponse> =>
+    request.get<ProductsApiResponse>(`/products?page=1&per_page=12`).then(resp => {
+      const filtered = resp.data.data.filter(val => val.name.indexOf(searchTerm) > -1)
+      const startInd = (page - 1) * itemsPerPage
+      return {
+        page,
+        per_page: itemsPerPage,
+        total: filtered.length,
+        total_pages: 8,
+        data: filtered.slice(startInd, startInd + itemsPerPage)
+      }
+    })
+}
+
+export default { Products }
